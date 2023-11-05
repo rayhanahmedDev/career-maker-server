1a7096a2c11f975e5880c06f173956953a863a2e
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-11-05 18:55:25 +0600

    get to data from mongodb

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.or5ssuz.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,12 +31,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const servicesCollection = client.db('careerDB').collection('career');
+
+    app.get('/services',async(req,res)=>{
+        const result = await servicesCollection.find().toArray()
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
